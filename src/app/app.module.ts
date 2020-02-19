f36e4a59094f8827882d07fecc13fc4113df9ca8
Mariajose <mariajosegomezcampo@gmail.com>
2020-02-19 14:24:01 -0500

    corriginedo errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { ReactiveFormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-=======
-/**** Formulario ****/ 
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule }   from '@angular/forms';
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
 
 //servicios
 import { AuthService } from './services/auth.service';
 
-<<<<<<< HEAD
 //Rutas
-=======
-//
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
 import { AppRoutingModule } from './app-routing.module';
 
 //componentes
@@ -32,10 +22,10 @@ import { PostulantsComponent } from './components/postulants/postulants.componen
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HeaderComponent } from './components/shared/header/header.component';
-<<<<<<< HEAD
+import { SelectComponent } from './components/user/select/select.component';
+import { ContenedorLoginComponent } from './components/user/contenedor-login/contenedor-login.component';
+import { ContenedorRegisterComponent } from './components/user/contenedor-register/contenedor-register.component';
 
-=======
->>>>>>> ac261d7129c453e7b5fe6d6ce5fb12dadbf0aec8
 
 //firebase
 import { AngularFireModule } from '@angular/fire'; // Firebase config
@@ -43,35 +33,14 @@ import { AngularFirestoreModule } from '@angular/fire/firestore'; // For Cloud F
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
-<<<<<<< HEAD
 // environment config
 import{ environment } from '../environments/environment';
-import { SelectComponent } from './components/user/select/select.component';
-import { ContenedorLoginComponent } from './components/user/contenedor-login/contenedor-login.component';
-import { ContenedorRegisterComponent } from './components/user/contenedor-register/contenedor-register.component';
-=======
-//firebase
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFireAuth } from '@angular/fire/auth';
-
-// environment config
-import{ environment } from '../environments/environment';
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
 
 @NgModule({
   declarations:[
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     RegisterComponent,
-=======
-    RegisterComponent
->>>>>>> ac261d7129c453e7b5fe6d6ce5fb12dadbf0aec8
-=======
-    RegisterComponent,
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
     HomeComponent,
     ProfileComponent,
     ManagementVacanciesComponent,
@@ -80,14 +49,10 @@ import{ environment } from '../environments/environment';
     NavBarComponent,
     FooterComponent,
     HeaderComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     SelectComponent,
     ContenedorLoginComponent,
     ContenedorRegisterComponent,
-    
-=======
->>>>>>> ac261d7129c453e7b5fe6d6ce5fb12dadbf0aec8
+
   ],
   imports: [
     BrowserModule,
@@ -100,26 +65,14 @@ import{ environment } from '../environments/environment';
   ],
   providers: [ 
     AngularFireAuth,
-    AuthService ],
-=======
+    AuthService ,
     NavBarComponent,
     FooterComponent,
     HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireDatabaseModule, 
-    
-  ],
-  providers: [
-   AngularFireAuth,
-   AuthService
-  ],
->>>>>>> 47b1ef15bfba49cdbad7ae41b78dd449a290ab16
+
+
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
